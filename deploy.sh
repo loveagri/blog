01977458ecf0db8e1219d3ef2510714ef28c0cb8
loveagri <1044531744@qq.com>
2020-11-04 16:10:25 +0700

    original

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env sh
 
 git add .
-git commit -m 'deploy'
+git commit -m 'original'
 git push
 
-# abort on errors
+# 确保脚本抛出遇到的错误
 set -e
 
-# build
-npm run docs:build
+# 生成静态文件
+npm run build
 
-# navigate into the build output directory
-cd docs
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
- echo 'blog.dotohi.com' > CNAME
+# deploy to github
+echo 'record.dotohi.com' > CNAME
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:loveagri/blog.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://loveagri:${GITHUB_TOKEN}@github.com/loveagri/blog.git
+  git config --global user.name "loveagri"
+  git config --
+  global user.email "1044531744@qq.com"
+fi
+
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:loveagri/blog.git master:gh-pages
-
-cd -
+cd - # 退回开始所在目录
+rm -rf docs/.vuepress/dist
