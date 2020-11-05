82f1845a5085ab51f763f04cb3682f472eafec34
loveagri <1044531744@qq.com>
2020-11-05 12:59:38 +0700

    original

--- docs/.vuepress/config/navConfig.js
+++ docs/.vuepress/config/navConfig.js
@@ -1,28 +1,7 @@
 module.exports = [
     {text: '首页', link: '/'},
-    {
-        text: '笔记',
-        link: '/note/',  //目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-        items: [
-            {
-                text: 'PHP',
-                items: [
-                    {text: 'PHP', link: '/note/javascript/'},
-                    {text: 'Python', link: '/note/es6/'},
-                ]
-            }
-        ]
-    },
-    {
-        text: '更多',
-        link: '/more/',
-        items: [
-            {text: '学习', link: '/pages/f2a556/'},
-            {text: '面试', link: '/pages/aea6571b7a8bae86/'},
-            {text: '心情杂货', link: '/pages/2d615df9a36a98ed/'},
-            {text: '友情链接', link: '/friends/'},
-        ]
-    },
+    {text: '技术栈', link: '/tech/'},
+    {text: '英文手记', link: '/english/'},
     {text: '关于', link: '/about/'},
     {
         text: '收藏',
