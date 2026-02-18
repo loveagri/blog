62d99bdff168f235a21981d3f13d5411de3199d7
loveagri <282656050@qq.com>
2026-02-18 23:34:51 +0800

    deploy

---
+++ assets/computer_Linux_DNS.md.BY0iOg34.js
@@ -0,0 +1,17 @@
+import{_ as n,c as a,o as l,ah as e}from"./chunks/framework.BrJYEL96.js";const d=JSON.parse('{"title":"DNS","description":"","frontmatter":{},"headers":[],"relativePath":"computer/Linux/DNS.md","filePath":"computer/Linux/DNS.md","lastUpdated":1754837914000}'),p={name:"computer/Linux/DNS.md"};function r(t,s,c,i,o,b){return l(),a("div",null,s[0]||(s[0]=[e(`<h1 id="dns" tabindex="-1">DNS <a class="header-anchor" href="#dns" aria-label="Permalink to &quot;DNS&quot;">​</a></h1><h2 id="国内常见dns" tabindex="-1">国内常见DNS <a class="header-anchor" href="#国内常见dns" aria-label="Permalink to &quot;国内常见DNS&quot;">​</a></h2><div class="language-sh line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki one-dark-pro vp-code" style="background-color:#282c34;color:#abb2bf;" tabindex="0"><code><span class="line"><span style="color:#7F848E;font-style:italic;">#114 DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：114.114.114.114、114.114.115.115</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#7F848E;font-style:italic;">#阿里云 DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：223.5.5.5、223.6.6.6</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#7F848E;font-style:italic;">#腾讯云 DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：119.29.29.29、119.28.28.28</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#7F848E;font-style:italic;">#CNNIC DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：1.2.4.8、210.2.4.8</span></span>
+<span class="line"><span style="color:#ABB2BF;">	</span></span>
+<span class="line"><span style="color:#7F848E;font-style:italic;">#百度 DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：180.76.76.76</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#7F848E;font-style:italic;">#Google DNS：</span></span>
+<span class="line"><span style="color:#61AFEF;">	IPv4：8.8.8.8、8.8.4.4</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br></div></div>`,3)]))}const m=n(p,[["render",r]]);export{d as __pageData,m as default};
---
+++ assets/computer_Linux_commands_seq.md.BRUjxGge.js
@@ -0,0 +1,24 @@
+import{_ as n,c as a,o as l,ah as p}from"./chunks/framework.BrJYEL96.js";const F=JSON.parse('{"title":"seq","description":"","frontmatter":{},"headers":[],"relativePath":"computer/Linux/commands/seq.md","filePath":"computer/Linux/commands/seq.md","lastUpdated":1754837914000}'),e={name:"computer/Linux/commands/seq.md"};function r(c,s,o,i,t,b){return l(),a("div",null,s[0]||(s[0]=[p(`<h1 id="seq" tabindex="-1">seq <a class="header-anchor" href="#seq" aria-label="Permalink to &quot;seq&quot;">​</a></h1><div class="language-sh line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki one-dark-pro vp-code" style="background-color:#282c34;color:#abb2bf;" tabindex="0"><code><span class="line"><span style="color:#61AFEF;">$</span><span style="color:#98C379;"> seq</span><span style="color:#D19A66;"> 10</span></span>
+<span class="line"><span style="color:#61AFEF;">1</span></span>
+<span class="line"><span style="color:#61AFEF;">2</span></span>
+<span class="line"><span style="color:#61AFEF;">3</span></span>
+<span class="line"><span style="color:#61AFEF;">4</span></span>
+<span class="line"><span style="color:#61AFEF;">5</span></span>
+<span class="line"><span style="color:#61AFEF;">6</span></span>
+<span class="line"><span style="color:#61AFEF;">7</span></span>
+<span class="line"><span style="color:#61AFEF;">8</span></span>
+<span class="line"><span style="color:#61AFEF;">9</span></span>
+<span class="line"><span style="color:#61AFEF;">10</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#61AFEF;">$</span><span style="color:#98C379;"> seq</span><span style="color:#D19A66;"> 10</span><span style="color:#D19A66;"> 20</span></span>
+<span class="line"><span style="color:#61AFEF;">10</span></span>
+<span class="line"><span style="color:#61AFEF;">11</span></span>
+<span class="line"><span style="color:#61AFEF;">12</span></span>
+<span class="line"><span style="color:#61AFEF;">13</span></span>
+<span class="line"><span style="color:#61AFEF;">14</span></span>
+<span class="line"><span style="color:#61AFEF;">15</span></span>
+<span class="line"><span style="color:#61AFEF;">16</span></span>
+<span class="line"><span style="color:#61AFEF;">17</span></span>
+<span class="line"><span style="color:#61AFEF;">18</span></span>
+<span class="line"><span style="color:#61AFEF;">19</span></span>
+<span class="line"><span style="color:#61AFEF;">20</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br></div></div>`,2)]))}const u=n(e,[["render",r]]);export{F as __pageData,u as default};
